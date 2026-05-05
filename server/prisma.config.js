2aa93c163b55930f573a2d11d9e6601883493a3d
sourabh-shrivastava33 <sourabhshrivastava3011@gmail.com>
2026-05-05 15:24:21 +0530

    fix issues

--- server/prisma.config.js
+++ server/prisma.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "prisma/config";
 import dotenv from "dotenv";
-dotenv.config({ path: '../.env' });
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: '../.env' });
+}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
